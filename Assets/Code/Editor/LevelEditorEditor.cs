91c73f722e6fcdf862252eb550969e0a281eea6c
Jeffrey Zacher <jeffmz705@gmail.com>
2018-07-03 22:39:05 -0500

    Create Platform Creator (LevelEditor), create custom unity editor to create and destroy the platform for ease of testing

---
+++ Assets/Code/Editor/LevelEditorEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(LevelEditor))]
+public class LevelEditorEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		LevelEditor levelEditor = (LevelEditor)target;
+
+		if (GUILayout.Button("Generate level"))
+		{
+			levelEditor.CreatePlatform(levelEditor.PlatfromCenterTile);
+		}
+
+		if (GUILayout.Button("Delete Levels"))
+		{
+			int j = levelEditor.levelParent.transform.childCount;
+			for (int i = 1; i < j; i++)
+			{
+				DestroyImmediate(levelEditor.levelParent.transform.GetChild(1).gameObject);
+			}
+		}
+	}
+}
--- Assets/Code/LevelEditor.cs
+++ Assets/Code/LevelEditor.cs
@@ -1,17 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LevelEditor : MonoBehaviour 
 {
+	public GameObject obstacle;
+	public GameObject floor;
+	public GameObject levelParent;
 
-	void Start () 
+	//IMPORTANT: Make sure that the level Prefabe is SQUARE & ODD NUMBERS!!!!!!!!!!!!
+	private int FloorWidth { get {return (int)floor.transform.localScale.x; } }
+	private int levelNumber { get { return levelParent.transform.childCount; } }
+
+	[SerializeField]
+	public Vector2Int PlatfromCenterTile;
+
+
+
+	public void CreatePlatform(Vector2Int platformCenter)
 	{
-		
+		PlatfromCenterTile = platformCenter;
+
+		var platformcontainer = new GameObject("Level" + PlatfromCenterTile.x + "x" + PlatfromCenterTile.y + "y");
+		platformcontainer.transform.parent = levelParent.transform;
+
+		//Create the floor .75 meters below 0 so that the obstaclys have a transform of 0 in the y-axis)
+		Instantiate(floor, new Vector3(platformCenter.x, 0f, platformCenter.y) * FloorWidth + new Vector3(0f, -.75f, 0f), Quaternion.identity, platformcontainer.transform);
+
+		var array = CreateObstacleGrid();
+
+		for (int i = 0; i < FloorWidth; i++)
+		{
+			for (int j = 0; j < FloorWidth; j++)
+			{
+				if (array[i, j])
+				{
+					var foo = GetObstaclePosition(i, j, PlatfromCenterTile * FloorWidth);
+					Instantiate(obstacle, new Vector3(foo.x, 0f, foo.y), Quaternion.identity, platformcontainer.transform);
+				}
+			}
+		}
 	}
-	
-	void Update () 
+
+	public void DestroyPlatform()
 	{
-		
+
+	}
+
+	public bool[,] CreateObstacleGrid()
+	{
+		bool[,] obstacles = new bool[FloorWidth, FloorWidth];
+		System.Random rnd = new System.Random();
+
+		for (int i = 0; i < FloorWidth; i++)
+		{
+			for (int j = 0; j < FloorWidth; j++)
+			{
+				obstacles[i, j] = rnd.Next(0, 2) == 1;
+			}
+		}
+		return obstacles;
+	}
+
+	public Vector2Int GetObstaclePosition(int x, int z, Vector2Int platformCenter)
+	{
+		return platformCenter
+			+ new Vector2Int(-4, -4)
+			+ new Vector2Int(x, z);
 	}
 }
